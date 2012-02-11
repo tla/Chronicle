51a0ea1f6551749f598991ecd7b34d1c01c73165
Tara L Andrews <tla\@mit.edu>
2012-02-11 23:04:49 +0100

    initial commit

---
+++ lib/Chronicle/Controller/Root.pm
@@ -0,0 +1,155 @@
+package Chronicle::Controller::Root;
+use Moose;
+use namespace::autoclean;
+
+BEGIN { extends 'Catalyst::Controller' }
+
+#
+# Sets the actions in this controller to be registered with no prefix
+# so they function identically to actions created in MyApp.pm
+#
+__PACKAGE__->config(namespace => '');
+
+=head1 NAME
+
+Chronicle::Controller::Root - Root Controller for Chronicle
+
+=head1 DESCRIPTION
+
+[enter your description here]
+
+=head1 METHODS
+
+=head2 index
+
+The root page (/)
+
+=cut
+
+sub index :Path :Args(0) {
+    my ( $self, $c ) = @_;
+	$c->stash->{'template'} = 'mainpage.tt2';
+	$c->stash->{'pagetitle'} = 'Excerpts from the Chronicle of Matthew of Edessa';
+	$c->forward('View::TT');
+}
+
+=head2 text/$id
+
+Display the text, translation, and apparatus for the given text excerpt.
+
+=cut
+
+sub text :Local :Args(1) {
+	my( $self, $c, $textid ) = @_;
+	my $m = $c->model('Edition');
+	$c->stash->{'textname'} = $m->textname( $textid );
+	$c->stash->{'paragraphs'} = $m->paragraphs( $textid );
+	$c->stash->{'template'} = 'editiontext.tt2';
+}
+
+=head2 witness/$wit
+
+Display the source text for the given witness.
+
+=cut
+
+sub witness :Chained('/') :PathPart :CaptureArgs(1) {
+	my( $self, $c, $witness ) = @_;
+	$c->stash->{'witness'} = $witness;
+}
+
+sub wit_display :PathPart('') :Chained('witness') :Args(0) {
+	my( $self, $c ) = @_;
+	$DB::single = 1;
+	my $wit = delete $c->stash->{'witness'};
+	my $m = $c->model('Witness');
+	$c->stash->{'pagetitle'} = 'Source view';
+	my $textdata = $m->as_html( $wit );
+	map { $c->stash->{$_} = $textdata->{$_} } keys %$textdata;
+	$c->stash->{'template'} = "witness.tt2";
+	$c->forward('View::TT');
+}
+
+=head2 witness/$wit/$format
+
+Return the source text for the given witness in the given format.
+
+=cut
+
+sub wit_return :PathPart('') :Chained('witness') :Args(1) {
+	my( $self, $c, $format ) = @_;
+	# TODO error unless format is a recognized one
+	my $m = $c->model('Witness');
+	my $sub = "as_$format";
+	$c->stash->{'result'} = $m->$sub( $c->stash->{'witness'} );
+	$c->forward( 'View::'.uc($format) );
+}
+
+=head2 abouttext
+
+=head2 aboutsite
+
+Return the static-but-for-templating page requested.
+
+=cut
+
+sub abouttext :Local :Args(0) {
+	my( $self, $c ) = @_;
+	$c->stash->{'template'} = 'abouttext.tt2';
+	$c->forward('View::TT');
+}
+
+sub aboutsite :Local :Args(0) {
+	my( $self, $c ) = @_;
+	$c->stash->{'template'} = 'aboutsite.tt2';
+	$c->forward('View::TT');
+}
+
+=head2 default
+
+Standard 404 error page
+
+=cut
+
+sub default :Path {
+    my ( $self, $c ) = @_;
+    $c->response->body( 'Page not found' );
+    $c->response->status(404);
+}
+
+=head2 auto
+
+Populate the stash with the things our wrapper needs.
+
+=cut
+
+sub auto :Private {
+	my( $self, $c ) = @_;
+	$c->stash->{'witnesses'} = $c->model('Witness')->get_witlist;
+	$c->stash->{'textlist'} = $c->model('Edition')->get_textlist;
+}
+
+=cut
+
+=head2 end
+
+Attempt to render a view, if needed.
+
+=cut
+
+sub end : ActionClass('RenderView') {}
+
+=head1 AUTHOR
+
+Tara L Andrews
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
+
+1;
---
+++ lib/Chronicle/Model/Witness.pm
@@ -0,0 +1,210 @@
+package Chronicle::Model::Witness;
+use Moose;
+use MooseX::Types::Path::Class qw/ Dir File /;
+use File::Basename qw/ fileparse /;
+use Text::TEI::Collate::Manuscript;
+use XML::LibXML;
+use XML::LibXML::XPathContext;
+use namespace::autoclean;
+
+extends 'Catalyst::Model';
+
+has 'sourcedir' => (
+	is => 'ro',
+	isa => Dir,
+	coerce => 1,
+	required => 1,
+	);
+	
+has 'sigilmap' => (
+	isa => 'HashRef[XML::LibXML::Document]',
+	traits => ['Hash'],
+	default => sub { {} },
+	handles => {
+		add_text => 'set',
+		textobj => 'get',
+		sigla => 'keys',
+		},
+	);
+	
+has 'sigilnamemap' => (
+	isa => 'HashRef[Str]',
+	traits => ['Hash'],
+	default => sub { {} },
+	handles => {
+		add_textname => 'set',
+		textname => 'get',
+		witpairs => 'kv',
+		},
+	);
+		
+sub BUILD {
+	my $self = shift;
+	# Read all the files in sourcedir, get their sigla, and add to sigilmap.
+	while( my $file = $self->sourcedir->next ) {
+		$self->_add_to_maps( $file );
+	}
+}
+
+sub get_witlist {
+	my $self = shift;
+	my $ret = [];
+	foreach my $pair ( sort { $a->[1] cmp $b->[1] } $self->witpairs ) {
+		push( @$ret, { 'sigil' => $pair->[0], 'name' => $pair->[1] } );
+	}
+	return $ret;
+}
+
+sub _add_to_maps {
+	my( $self, $file ) = @_;
+	my( $fn, $fp, $fs ) = fileparse( $file->absolute, qr/\.[^.]*/ );
+	return unless $fs eq '.xml';
+	my $parser = XML::LibXML->new();
+	my $doc = $parser->parse_file( "$file" );
+	my $xpc = _xpc_for_el( $doc->documentElement );
+	my $sigil = $xpc->findvalue( '//tei:msDesc/attribute::xml:id' );
+	my $textname = sprintf( "%s, %s %s", 
+		$xpc->findvalue( '//tei:msIdentifier/tei:settlement' ),
+		$xpc->findvalue( '//tei:msIdentifier/tei:repository' ),
+		$xpc->findvalue( '//tei:msIdentifier/tei:idno' ) );
+	$self->add_text( $sigil, $doc );
+	$self->add_textname( $sigil, $textname );
+}
+
+sub as_html {
+	my( $self, $sigil ) = @_;
+	my $textroot = $self->textobj( $sigil );
+	# Name, that's easy
+	my $return_hash = { 'textidentifier' => $self->textname( $sigil ) };
+	my $xpc = _xpc_for_el( $textroot );
+	# Description blurb also not too hard
+	$return_hash->{'textdescription'} = join( '',
+		map { $_->toString } $xpc->findnodes( '//tei:msDesc/tei:p' ) );
+	# Now comes the fun part - parse the body.
+	$return_hash->{'textcontent'} = _html_transform( 
+		$xpc->findnodes( '/tei:TEI/tei:text/tei:body' ),
+		$xpc->exists( '//tei:cb' ) );
+	return $return_hash;
+}
+
+sub as_json {
+	my( $self, $sigil ) = @_;
+	my $msobj = Text::TEI::Collate::Manuscript->new( 
+		'sourcetype' => 'xmldesc',
+		'source' => $self->textobj( $sigil )->documentElement,
+		);
+	return $msobj->tokenize_as_json();
+}
+
+sub as_tei {
+	my( $self, $sigil ) = @_;
+	return $self->textobj( $sigil )->toString(1);
+}
+
+sub _html_transform {
+	my( $element, $usecolumns ) = @_;
+	my %span_map = (
+		'add' => 'addition',
+		'del' => 'deletion',
+		'num' => 'number',
+		'hi' => 'highlight',
+		'ex' => 'expansion',
+		'expan' => 'expansion',
+		);
+	my @return_words;
+	## NONRECURSING ELEMENTS
+	if( $element->nodeType == XML_TEXT_NODE ) {
+		my $text = $element->data;
+		$text =~ s/^\s+//gs;
+		$text =~ s/\s+$//gs;
+		push( @return_words, $text );
+	} elsif( $element->nodeName eq 'w' ) {
+		# Simple word, just the text content.
+		push( @return_words, $element->textContent . ' ' );
+	} elsif( $element->nodeName eq 'lb' ) {
+		push( @return_words, '<br/>' );
+	} elsif( $element->nodeName eq 'damage' ) {
+		my $len = $element->getAttribute('extent');
+		push( @return_words, 'X' x $len );
+
+		
+	## RECURSING ELEMENTS
+	} elsif( $element->nodeName =~ /^(body|seg|subst)$/ ) {
+		# No wrapping, just pass-through
+		@return_words = map { _html_transform( $_, $usecolumns ) } $element->childNodes;
+		# but if it's a segword, put in a space.
+		push( @return_words, ' ' ) if $element->nodeName eq 'seg';
+	} elsif( $element->nodeName eq 'div' ) {
+		# Section marker, then recurse
+		my $secnum = $element->hasAttribute('n') ? $element->getAttribute('n') : '';
+		push( @return_words, sprintf( "<div class=\"section\">\x{A7} %s</div>", $secnum ) );
+		push( @return_words, map { _html_transform( $_, $usecolumns ) } $element->childNodes );
+		
+	} elsif( exists $span_map{$element->nodeName} ) {
+		# Span wrapping
+		my $spantype = $span_map{$element->nodeName};
+		push( @return_words, "<span class=\"$spantype\">" );
+		push( @return_words, map { _html_transform( $_, $usecolumns ) } $element->childNodes );
+		push( @return_words, '</span>' );
+		
+	} elsif( $element->nodeName eq 'p' ) {
+		# Paragraph wrapping
+		push( @return_words, '<p>' );
+		push( @return_words, map { _html_transform( $_, $usecolumns ) } $element->childNodes );
+		push( @return_words, '</p>' );
+
+	## OTHER ELEMENTS
+	} elsif( $element->nodeName eq 'pb' ) {
+		# Now we get to the more complicated one.
+		# Close the preceding paragraph if necessary, then a div, then reopen
+		# a paragraph if necessary.
+		my $xpc = _xpc_for_el( $element );
+		my $midpg = $xpc->exists( 'ancestor::tei:p' );
+		push( @return_words, '</p>' ) if $midpg;
+		push( @return_words, '</td></tr></table>' ) if $usecolumns;
+		push( @return_words, '<div class="pagenum">' );
+		push( @return_words, $element->getAttribute('n') );
+		push( @return_words, '</div>' );
+		push( @return_words, '<table class="pagecolumn"><tr><td class="left">' )
+			if $usecolumns;
+		push( @return_words, '<p class="followpg">' ) if $midpg;
+	} elsif( $element->nodeName eq 'cb' ) {
+		## usecolumns had better be 1.
+		my $xpc = _xpc_for_el( $element );
+		my $midpg = $xpc->exists( 'ancestor::tei:p' );
+		push( @return_words, '</p>' ) if $midpg;
+		push( @return_words, '</td><td class="right">' );
+		push( @return_words, '<p class="followpg">' ) if $midpg;
+	}
+	
+	return join( '', @return_words );
+}
+sub _xpc_for_el {
+	my $el = shift;
+	my $xpc = XML::LibXML::XPathContext->new( $el );
+	$xpc->registerNs( 'tei', 'http://www.tei-c.org/ns/1.0' );
+	return $xpc;
+}	
+
+=head1 NAME
+
+Chronicle::Model::Witness - Catalyst Model
+
+=head1 DESCRIPTION
+
+Catalyst Model.
+
+=head1 AUTHOR
+
+Tara L Andrews
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
+
+1;
